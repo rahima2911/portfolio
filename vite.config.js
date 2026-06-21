da8067615294d2e832d659068a8443431c0c32d4
Rahima <rahimasarwar29@gmail.com>
2026-06-21 21:33:55 +0500

    vite.config file updated for github

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // If you deploy to https://<username>.github.io/<repo-name>/ uncomment and set base:
-  // base: '/your-repo-name/',
-})
+  base: "/portfolio/", // Humne aapki repo ka naam 'portfolio' yahan set kar diya hai
+});
