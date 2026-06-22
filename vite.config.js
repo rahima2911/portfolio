4029bf7da33a42b9e1c27d80bc1b0384c9fcdf3e
Rahima <rahimasarwar29@gmail.com>
2026-06-22 16:15:47 +0500

    Add Google Search Console verification meta tag

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio/", // Humne aapki repo ka naam 'portfolio' yahan set kar diya hai
+  base: "/portfolio/",
 });
